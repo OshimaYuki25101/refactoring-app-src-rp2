ff18139e9d4a05481248b41f7eff4ed3da88292e
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-04 16:41:35 +0900

    fix:ConstantValue.java 定数の作成
    case文のメニュー番号を定数化
    <命名規則>
    MENU_(機能名)
    
    性別の番号を定数化
    <命名規則>
    GENDER_(項目)
    
    部署の番号を定数化
    <命名規則>
    (部署)_DEPT

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantValue.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantValue.java
@@ -4,4 +4,20 @@ public class ConstantValue {
 	private ConstantValue() {
 	}
 
+	public static final int MENU_FIND_ALL = 1;
+	public static final int MENU_FIND_BY_NAME = 2;
+	public static final int MENU_FIND_BY_DEPTID = 3;
+	public static final int MENU_INSERT_EMPLOYEE = 4;
+	public static final int MENUUPDATE_EMPLOYEE = 5;
+	public static final int MENU_DELETE_EMPLOYEE = 6;
+	public static final int MENU_SYSTEM_END = 7;
+	
+	public static final int GENDER_NO_ANSWER = 0;
+	public static final int GENDER_MAN = 1;
+	public static final int GENDER_WOMAN = 2;
+	public static final int GENDER_OTHERS = 9;
+	
+	public static final int SALES_DEPT = 1;
+	public static final int MANAGEMENT_DEPT = 2;
+	public static final int GENERAL_AFFAIRS_DEPT = 3;
 }

0195f40d04815fd487e4bf02c38595eae32d94fd
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-04 16:45:03 +0900

    fix:ConstantValue.java 定数名の修正
    「MENUUPDATE_EMPLOYEE」から「MENU_UPDATE_EMPLOYEE」に修正

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantValue.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantValue.java
@@ -8,7 +8,7 @@ public class ConstantValue {
 	public static final int MENU_FIND_BY_NAME = 2;
 	public static final int MENU_FIND_BY_DEPTID = 3;
 	public static final int MENU_INSERT_EMPLOYEE = 4;
-	public static final int MENUUPDATE_EMPLOYEE = 5;
+	public static final int MENU_UPDATE_EMPLOYEE = 5;
 	public static final int MENU_DELETE_EMPLOYEE = 6;
 	public static final int MENU_SYSTEM_END = 7;
 	

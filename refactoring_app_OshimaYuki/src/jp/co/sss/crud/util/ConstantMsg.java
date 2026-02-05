f63ccbc3fc59e455b388e66b9a3b333dc70a6f59
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-05 15:31:58 +0900

    add:ConstantMsg.java 定数の追加
    メッセージ表示の定数を追加

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantMsg.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -5,6 +5,18 @@ public class ConstantMsg {
 	private ConstantMsg(){
 	}
 	
-//	public static final String 
+	//メニュー名
+	public static final String MENU_TAITOL = "=== 社員管理システム ===";
+	public static final String MANU_FIND_ALL = "1.全件表示";
+	
+	public static final String EMPLOYEE_NAME = "社員名:";
+	
+	public static final String NOT_FIND = "該当者はいませんでした";
+	public static final String COLUMN_NAME = "社員ID\t社員名\t性別\t生年月日\t部署名";
+	public static final String DELETE_EMPLOYEE = "社員情報を削除しました";
 
+	public static final String UPDATE_EMP_ID = "更新する社員の社員IDを入力してください：";
+	public static final String UPDATE_COMPLETE = "社員情報を更新しました";
+	public static final String DELETE_EMP_ID = "削除する社員の社員IDを入力してください：";
+	public static final String SYSTEM_END = "システムを終了します。";
 }

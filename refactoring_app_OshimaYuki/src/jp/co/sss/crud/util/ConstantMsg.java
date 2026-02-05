5863301059bd4728f3a432db3952f6ac5cc73257
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-05 16:34:58 +0900

    fix:ConstantMsg.java メッセージ内容の変更
    ・「社員名:」の「:」を削除
    ・性別のメッセージを追加
    　「GENDER_<性別>」に統一

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantMsg.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -17,7 +17,7 @@ public class ConstantMsg {
 	public static final String MENU_NUMBER_QUESTION 		= "メニュー番号を入力してください：";
 
 	
-	public static final String EMPLOYEE_NAME 				= "社員名:";
+	public static final String EMPLOYEE_NAME 				= "社員名";
 	public static final String GENDER_TYPE 				= "性別(0:その他, 1:男性, 2:女性, 9:回答なし)";
 	public static final String BIRTHDAY 					= "生年月日(西暦年/月/日)";
 	public static final String DEPT_TYPE 					= "部署ID(1:営業部、2:経理部、3:総務部)";
@@ -30,4 +30,9 @@ public class ConstantMsg {
 	public static final String UPDATE_COMPLETE 			= "社員情報を更新しました";
 	public static final String DELETE_EMP_ID 				= "削除する社員の社員IDを入力してください：";
 	public static final String SYSTEM_END 					= "システムを終了します。";
+	
+	public static final String GENDER_NO_ANSWER 					= "回答なし";
+	public static final String GENDER_MAN 							= "男性";
+	public static final String GENDER_WOMAN  						= "女性";
+	public static final String GENDER_OTHRES 						= "その他";
 }

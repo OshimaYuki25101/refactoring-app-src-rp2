7f9e8d6075309aff10360ea3f37264424a109202
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 09:12:20 +0900

    fix:EmployeeDeleteService.java
    目的:表示の見た目の向上
    メッセージの後ろに「:」を追加しました

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeDeleteService.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeDeleteService.java
@@ -5,10 +5,15 @@ import jp.co.sss.crud.io.EmployeeEmpIdReader;
 import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeDeleteService {
+	
 	public static void EmpDelete() {
-		System.out.print(ConstantMsg.DELETE_EMP_ID);
+		
+		System.out.print(ConstantMsg.DELETE_EMP_ID+":");
+		
 		EmployeeEmpIdReader inputEmpId = new EmployeeEmpIdReader();
+		
 		int empId = (int) inputEmpId.input();
+		
 		EmployeeDAO.deleteEmployee(empId);
 	}
 }

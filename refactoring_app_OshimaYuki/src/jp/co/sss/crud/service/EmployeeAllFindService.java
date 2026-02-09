893962d9ad8cc6bb678a6e6b9b86b7029018d412
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 13:06:17 +0900

    fix:EmployeeAllFindService.java throws文の変更
    「IllegalInputException」「SystemErrorException」に変更

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeAllFindService.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeAllFindService.java
@@ -1,15 +1,15 @@
 package jp.co.sss.crud.service;
 
-import java.sql.SQLException;
-import java.text.ParseException;
 import java.util.List;
 
 import jp.co.sss.crud.db.EmployeeDAO;
 import jp.co.sss.crud.dto.Employee;
+import jp.co.sss.crud.exception.IllegalInputException;
+import jp.co.sss.crud.exception.SystemErrorException;
 import jp.co.sss.crud.io.ConsoleWriter;
 
 public class EmployeeAllFindService {
-	public static void findAll() throws ClassNotFoundException, SQLException, ParseException {
+	public static void findAll() throws IllegalInputException, SystemErrorException {
 		List<Employee> employees = EmployeeDAO.findAll();
 		ConsoleWriter.showEmployees(employees);
 	}

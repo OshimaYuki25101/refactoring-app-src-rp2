836a440224f1fe5e324029db66513530c73283ec
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 14:40:14 +0900

    fix:EmployeeDeleteService.java 例外処理の実装
    目的：信頼性の向上
    入力用のメソッド名を修正

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeDeleteService.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeDeleteService.java
@@ -1,18 +1,17 @@
 package jp.co.sss.crud.service;
 
 import jp.co.sss.crud.db.EmployeeDAO;
+import jp.co.sss.crud.exception.IllegalInputException;
+import jp.co.sss.crud.exception.SystemErrorException;
 import jp.co.sss.crud.io.EmployeeEmpIdReader;
-import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeDeleteService {
 	
-	public static void EmpDelete() {
-		
-		System.out.print(ConstantMsg.DELETE_EMP_ID+":");
+	public static void EmpDelete() throws IllegalInputException,SystemErrorException{
 		
 		EmployeeEmpIdReader inputEmpId = new EmployeeEmpIdReader();
 		
-		int empId = (int) inputEmpId.input();
+		int empId = (int) inputEmpId.inputEmpId();
 		
 		EmployeeDAO.deleteEmployee(empId);
 	}

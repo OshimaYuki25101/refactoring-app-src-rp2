03cec407be3fe98ef64c2298feee0b3d9435ae94
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 13:47:14 +0900

    fix:EmployeeFindByDeptIdService.java 例外処理の修正
    目的：可読性の向上
    try{}catch文をServiceではなく、ioで処理するよう修正

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeFindByDeptIdService.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeFindByDeptIdService.java
@@ -8,27 +8,11 @@ import jp.co.sss.crud.exception.IllegalInputException;
 import jp.co.sss.crud.exception.SystemErrorException;
 import jp.co.sss.crud.io.ConsoleWriter;
 import jp.co.sss.crud.io.EmployeeDeptIdReader;
-import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeFindByDeptIdService {
 	public static void findByDeptId() throws IllegalInputException, SystemErrorException {
-		int deptId = 0;
-		boolean isError = false;
-		do {
-			try {
-				System.out.print(ConstantMsg.DEPT_TYPE + "を入力してください:");
-				EmployeeDeptIdReader inputDeptId = new EmployeeDeptIdReader();
-				deptId = (int) inputDeptId.input();
-			} catch (IllegalInputException e) {
-				System.out.println(e.getMessage());
-				System.out.println();
-				isError = true;
-			}catch(SystemErrorException e) {
-				System.out.println(e.getMessage());
-				e.printStackTrace();
-				break;
-			}
-		} while (isError);
+		EmployeeDeptIdReader inputDeptId = new EmployeeDeptIdReader();
+		int deptId = inputDeptId.inputDeptId();
 		List<Employee> employees = EmployeeDAO.findByDeptId(deptId);
 		ConsoleWriter.showEmployees(employees);
 	}

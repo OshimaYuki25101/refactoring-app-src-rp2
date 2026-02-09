5aebf2c1340ea7f1e5a75ec0b74c1b4a81153e77
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 11:58:17 +0900

    fix:EmployeeDepiIdReader.java 入力チェックの実装
    目的：信頼性の向上
    1以上3以下での入力チェックを実装

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeDeptIdReader.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeDeptIdReader.java
@@ -1,22 +1,21 @@
 package jp.co.sss.crud.io;
 
-public class EmployeeDeptIdReader implements IConsoleReader{
+import jp.co.sss.crud.util.ConstantValue;
+
+public class EmployeeDeptIdReader implements IConsoleReader {
 
 	@Override
 	public String getErrorMsg() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return ConstantValue.DEPTID_MIN + "以上" + ConstantValue.DEPTID_MAX + "以下の整数を入力してください";
 	}
 
 	@Override
 	public boolean isValid(String inputString) {
-		// TODO 自動生成されたメソッド・スタブ
-		return true;
+		return inputString.matches("^[1-3１-３]{1}$");//1-3 までの数値
 	}
 
 	@Override
 	public boolean isParseInt() {
-		// TODO 自動生成されたメソッド・スタブ
 		return true;
 	}
 

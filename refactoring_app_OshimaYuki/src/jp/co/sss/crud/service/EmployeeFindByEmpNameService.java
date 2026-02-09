7bf194a52196bb148ac9c06dea4dbe70403fcd74
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 11:55:09 +0900

    fix:EmployeeFindByNameService.java 入力待ちを実装
    目的：信頼性も向上
    入力エラー後に再度入力できるよう入力待ちを実装

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeFindByEmpNameService.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeFindByEmpNameService.java
@@ -1,21 +1,34 @@
 package jp.co.sss.crud.service;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.text.ParseException;
 import java.util.List;
 
 import jp.co.sss.crud.db.EmployeeDAO;
 import jp.co.sss.crud.dto.Employee;
+import jp.co.sss.crud.exception.IllegalInputException;
+import jp.co.sss.crud.exception.SystemErrorException;
 import jp.co.sss.crud.io.ConsoleWriter;
 import jp.co.sss.crud.io.EmployeeNameReader;
 import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeFindByEmpNameService {
-	public static void findByEmpName() throws ClassNotFoundException, SQLException, IOException, ParseException {
-		System.out.print(ConstantMsg.EMPLOYEE_NAME+":");
-		EmployeeNameReader inputName = new EmployeeNameReader();
-		String empName = (String) inputName.input();
+	public static void findByEmpName() throws IllegalInputException, SystemErrorException {
+		String empName = "";
+		boolean isError = false;
+		do {
+			try {
+				System.out.print(ConstantMsg.EMPLOYEE_NAME + ":");
+				EmployeeNameReader inputName = new EmployeeNameReader();
+				empName = (String) inputName.input();
+			} catch (IllegalInputException e) {
+				System.out.println(e.getMessage());
+				System.out.println();
+				isError = true;
+			} catch (SystemErrorException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				break;
+			}
+		} while (isError);
 		List<Employee> employees = EmployeeDAO.findByName(empName);
 		ConsoleWriter.showEmployees(employees);
 	}

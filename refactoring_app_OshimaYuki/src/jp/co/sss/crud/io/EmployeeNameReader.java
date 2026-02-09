1b369760227c4ebd9bce025302672c06b7f4aca4
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 11:29:12 +0900

    fix:EMployeeNameReader.java　入力チェックの実装
    目的：信頼性の向上
    1文字以上30文字以下で入力チェックを実装

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeNameReader.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeNameReader.java
@@ -1,22 +1,25 @@
 package jp.co.sss.crud.io;
 
-public class EmployeeNameReader implements IConsoleReader{
+import jp.co.sss.crud.util.ConstantValue;
+
+public class EmployeeNameReader implements IConsoleReader {
 
 	@Override
 	public String getErrorMsg() {
-		
-		return null;
+		return ConstantValue.NAME_MIN + "文字以上" + ConstantValue.NAME_MAX + "文字以下の文字列を入力してください";
 	}
 
 	@Override
 	public boolean isValid(String inputString) {
-		// TODO 自動生成されたメソッド・スタブ
-		return true;
+		if (ConstantValue.NAME_MIN <= inputString.length() && inputString.length() <= ConstantValue.NAME_MAX) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean isParseInt() {
-		// TODO 自動生成されたメソッド・スタブ
 		return false;
 	}
 

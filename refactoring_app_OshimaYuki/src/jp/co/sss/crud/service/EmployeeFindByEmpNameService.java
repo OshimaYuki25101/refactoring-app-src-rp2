05f658fc47200546f66d926f375dbf590388fd57
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 13:53:11 +0900

    fix:EmployeeFindByEmpNameService.java 例外処理の修正
    目的：可読性の向上
    try{}catch文をServiceではなく、ioで処理するよう修正

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeFindByEmpNameService.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/service/EmployeeFindByEmpNameService.java
@@ -8,27 +8,11 @@ import jp.co.sss.crud.exception.IllegalInputException;
 import jp.co.sss.crud.exception.SystemErrorException;
 import jp.co.sss.crud.io.ConsoleWriter;
 import jp.co.sss.crud.io.EmployeeNameReader;
-import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeFindByEmpNameService {
 	public static void findByEmpName() throws IllegalInputException, SystemErrorException {
-		String empName = "";
-		boolean isError = false;
-		do {
-			try {
-				System.out.print(ConstantMsg.EMPLOYEE_NAME + ":");
-				EmployeeNameReader inputName = new EmployeeNameReader();
-				empName = (String) inputName.input();
-			} catch (IllegalInputException e) {
-				System.out.println(e.getMessage());
-				System.out.println();
-				isError = true;
-			} catch (SystemErrorException e) {
-				System.out.println(e.getMessage());
-				e.printStackTrace();
-				break;
-			}
-		} while (isError);
+		EmployeeNameReader inputName = new EmployeeNameReader();
+		String empName = inputName.inputName();
 		List<Employee> employees = EmployeeDAO.findByName(empName);
 		ConsoleWriter.showEmployees(employees);
 	}

d3534e781cf2705f84d2003ef55a00f3a72f3318
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 13:47:38 +0900

    fix:EmployeeDeptIdReader.java 例外処理の修正
    目的：可読性の向上
    try{}catch文をServiceではなく、ioで処理するよう修正

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeDeptIdReader.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeDeptIdReader.java
@@ -1,8 +1,32 @@
 package jp.co.sss.crud.io;
 
+import jp.co.sss.crud.exception.IllegalInputException;
+import jp.co.sss.crud.exception.SystemErrorException;
+import jp.co.sss.crud.util.ConstantMsg;
 import jp.co.sss.crud.util.ConstantValue;
 
 public class EmployeeDeptIdReader implements IConsoleReader {
+
+	public int inputDeptId() throws SystemErrorException, IllegalInputException {
+		int inputDeptId = ConstantValue.INITIAL_VALUE;
+		boolean isError = false;
+		do {
+			try {
+				isError = false;
+				System.out.print(ConstantMsg.DEPT_TYPE + "を入力してください:");
+				inputDeptId = (int) IConsoleReader.super.input();
+			} catch (IllegalInputException e) {
+				System.out.println(e.getMessage());
+				System.out.println();
+				isError = true;
+			} catch (SystemErrorException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				break;
+			}
+		} while (isError);
+		return inputDeptId;
+	}
 
 	@Override
 	public String getErrorMsg() {

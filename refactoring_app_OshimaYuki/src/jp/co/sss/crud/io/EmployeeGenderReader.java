bcb215e6e4bf27f54dfc1a47cb99d1dd73117bf0
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 14:03:18 +0900

    fix:EmployeeGenderReader.java 例外処理の追加
    目的：	信頼性の向上
    メッセージ、例外の追加

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeGenderReader.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/io/EmployeeGenderReader.java
@@ -1,22 +1,45 @@
 package jp.co.sss.crud.io;
 
-public class EmployeeGenderReader implements IConsoleReader{
+import jp.co.sss.crud.exception.IllegalInputException;
+import jp.co.sss.crud.exception.SystemErrorException;
+import jp.co.sss.crud.util.ConstantMsg;
+import jp.co.sss.crud.util.ConstantValue;
+
+public class EmployeeGenderReader implements IConsoleReader {
+
+	public int inputGender() {
+		int gender = ConstantValue.INITIAL_VALUE;
+		boolean isError = false;
+		do {
+			try {
+				isError = false;
+				System.out.print(ConstantMsg.GENDER_TYPE + ":");
+				gender = (int) IConsoleReader.super.input();
+			} catch (IllegalInputException e) {
+				System.out.println(e.getMessage());
+				System.out.println();
+				isError = true;
+			} catch (SystemErrorException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				break;
+			}
+		} while (isError);
+		return gender;
+	}
 
 	@Override
 	public String getErrorMsg() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return ConstantValue.GENDER_NO_ANSWER+"以上"+ConstantValue.GENDER_WOMAN+"以下、もしくは"+ConstantValue.GENDER_OTHERS+"で入力してください";
 	}
 
 	@Override
 	public boolean isValid(String inputString) {
-		// TODO 自動生成されたメソッド・スタブ
-		return true;
+		return inputString.matches("^|[0129０１２９]{1}$");
 	}
 
 	@Override
 	public boolean isParseInt() {
-		// TODO 自動生成されたメソッド・スタブ
 		return true;
 	}
 

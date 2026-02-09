9e9b7afd1faabcfc8d23c6b6588380e128da4579
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 12:01:21 +0900

    fix:SystemErrorException.java 例外処理の実装
    目的：信頼性の向上
    システムエラー時の例外処理を実装

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/exception/SystemErrorException.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/exception/SystemErrorException.java
@@ -1,33 +1,13 @@
 package jp.co.sss.crud.exception;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.security.cert.Extension;
+public class SystemErrorException extends Exception{
 
-public class SystemErrorException implements Extension{
-
-	@Override
-	public String getId() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
-
-	@Override
-	public boolean isCritical() {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
-	}
-
-	@Override
-	public byte[] getValue() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
-
-	@Override
-	public void encode(OutputStream out) throws IOException {
-		// TODO 自動生成されたメソッド・スタブ
+	public SystemErrorException() {
 		
+	}
+	
+	public SystemErrorException(String message, Exception e) {
+		super(message);
 	}
 
 }

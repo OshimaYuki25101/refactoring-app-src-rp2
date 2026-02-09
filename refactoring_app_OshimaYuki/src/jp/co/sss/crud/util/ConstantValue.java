3156ccd767dfe6c6019ef36143741089f8ff6616
OshimaYuki25101 <y.oshima@sun-bit.co.jp>
2026-02-09 14:41:24 +0900

    fix:ConstantValue.java 定数の追加
    目的：拡張性の向上
    エラーメッセージの定数を追加

--- refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantValue.java
+++ refactoring_app_OshimaYuki/src/jp/co/sss/crud/util/ConstantValue.java
@@ -4,8 +4,10 @@ public class ConstantValue {
 	private ConstantValue() {
 	}
 
+	//汎用
+	//初期値用
+	public static final int INITIAL_VALUE = 0;
 	//メニュー
-	public static final int MENU_START = 0;
 	//全件検索
 	public static final int MENU_FIND_ALL = 1;
 	//社員名検索
@@ -19,8 +21,8 @@ public class ConstantValue {
 	//削除
 	public static final int MENU_DELETE_EMPLOYEE = 6;
 	//終了
-	public static final int MENU_SYSTEM_END = 7;
-	
+	public static final int MENU_END = 7;
+
 	//性別用
 	//回答なし
 	public static final int GENDER_NO_ANSWER = 0;
@@ -30,7 +32,7 @@ public class ConstantValue {
 	public static final int GENDER_WOMAN = 2;
 	//その他
 	public static final int GENDER_OTHERS = 9;
-	
+
 	//部署ID用
 	//営業部
 	public static final int SALES_DEPT = 1;
@@ -38,4 +40,18 @@ public class ConstantValue {
 	public static final int ACCUNTING_DEPT = 2;
 	//総務部
 	public static final int GENERAL_AFFAIRS_DEPT = 3;
+
+	//エラーメッセージ用
+	//社員ID最小値
+	public static final int EMP_ID_MIN = 1;
+	//社員ID最大値
+	public static final int EMP_ID_MAX = 9999;
+	//文字数最小値
+	public static final int NAME_MIN = 1;
+	//文字数最大値
+	public static final int NAME_MAX = 30;
+	//部署ID最小値
+	public static final int DEPT_ID_MIN = 1;
+	//部署ID最大値
+	public static final int DEPT_ID_MAX = 3;
 }
